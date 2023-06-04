a2715f251519d9d888fce5480ad40bed6eb002f0
MatheusMamedio <matheusmamedio5@gmail.com>
2023-06-04 19:07:38 -0300

    adicionado exemplos de operadores aritmeticos vistos na aula

---
+++ Learning_Java/src/com/cursoloiane/matheus/aulas/OperadoresAritmeticos.java
@@ -0,0 +1,41 @@
+package com.cursoloiane.matheus.aulas;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		int resultado = 1+1;
+		System.out.println(resultado);
+		
+		resultado = resultado -1;
+		System.out.println(resultado);
+		
+		resultado = resultado *2;
+		System.out.println(resultado);
+		
+		resultado = resultado/2;
+		System.out.println(resultado);
+		
+		resultado = resultado + 8;
+		System.out.println(resultado);
+		
+		resultado = resultado % 7;
+		System.out.println(resultado);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
