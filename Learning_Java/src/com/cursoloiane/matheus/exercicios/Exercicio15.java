b8d8ca38baf676722e1674436d2d4dd94ecf41b0
MatheusMamedio <matheusmamedio5@gmail.com>
2023-06-06 02:15:09 -0300

    exercicio 15 resolvido !

---
+++ Learning_Java/src/com/cursoloiane/matheus/exercicios/Exercicio15.java
@@ -0,0 +1,28 @@
+package com.cursoloiane.matheus.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio15 {
+
+	public static void main(String[] args) {
+		/* Exercicio 15, faça um programa que peça dois numeros
+		 * e imprima o maior deles
+		 */
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Entre com o primeiro numero:");
+		int num1= scan.nextInt();
+		
+		System.out.println("Entre com o segundo numero:");
+		int num2= scan.nextInt();
+		
+		if(num1 > num2) {
+			System.out.println("o numero "+num1+" é maior");
+		}else System.out.println("o numero "+num2+" é maior");
+			
+		
+		
+
+	}
+
+}
