648e4d48f60e88c92d22b59f9a8e934f9b6dcc2f
MatheusMamedio <matheusmamedio5@gmail.com>
2023-06-06 02:21:03 -0300

    adicionado o exercicio 2

---
+++ Learning_Java/src/com/cursoloiane/matheus/exercicios/Exercicio16.java
@@ -0,0 +1,46 @@
+package com.cursoloiane.matheus.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio16 {
+
+	public static void main(String[] args) {
+		/*
+		 * exercicio 16, faça um programa que peça um valor e mostre na tela se o valor
+		 * é positivo ou negativo
+		 */
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Entre com o primeiro numero:");
+		int numero= scan.nextInt();
+		
+		
+		
+		if (numero >= 0 ) {
+			System.out.println("o numero é positivo");
+		}else {
+			System.out.println("o numero é negativo");
+		}
+		
+		scan.close();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
