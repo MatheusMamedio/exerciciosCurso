3630f59a33d64af137e35eac096d42616647f5b7
MatheusMamedio <matheusmamedio5@gmail.com>
2023-06-09 17:26:13 -0300

    adicionado exercicio 37

---
+++ Learning_Java/src/com/cursoloiane/matheus/exercicios/Exercicio37.java
@@ -0,0 +1,69 @@
+package com.cursoloiane.matheus.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio37 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("entre com o tipo da carne");
+		System.out.println("1 - file duplo");
+		System.out.println("2 - alcatra");
+		System.out.println("3 - picanha");
+		int tipo = scan.nextInt();
+
+		System.out.println("entre com a quantidade em Kg");
+		double qtd = scan.nextDouble();
+
+		double precoKg = 0;
+
+		if (tipo == 1) {
+
+			System.out.println(qtd + "Kg - File duplo");
+
+			if (qtd < 5) {
+
+				precoKg = 4.9;
+			} else {
+				precoKg = 5.8;
+			}
+		} else if (tipo == 2) {
+
+			System.out.println(qtd + "Kg - alcatra");
+
+			if (qtd < 5) {
+				precoKg = 5.9;
+			} else {
+				precoKg = 6.8;
+			}
+
+		} else if (tipo == 3) {
+
+			System.out.println(qtd + "Kg - picanha");
+
+			if (qtd < 5) {
+				precoKg = 5.9;
+			} else {
+				precoKg = 7.8;
+			}
+
+		}
+		double total = qtd * precoKg;
+		System.out.println(qtd + "Kg*" + precoKg + " - "+ total);
+
+		System.out.println("Compra no cartão? digite [1] para sim ");
+		int cartao = scan.nextInt();
+
+		if (cartao == 1) {
+			double desconto = (total / 100) * 5;
+			System.out.println("desconto de: " + desconto);
+			System.out.println("Valor a pagar: " + (total - desconto));
+		} else {
+			System.out.println("Valor a pagar: " + total);
+		}
+
+	}
+
+}
