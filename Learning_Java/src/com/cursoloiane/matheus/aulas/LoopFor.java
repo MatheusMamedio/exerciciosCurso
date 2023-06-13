30b3fd22224a097ebd354d9869fc41a5a3165f79
MatheusMamedio <matheusmamedio5@gmail.com>
2023-06-12 22:30:17 -0300

    adicionado exemplo sobre For visto na aula

---
+++ Learning_Java/src/com/cursoloiane/matheus/aulas/LoopFor.java
@@ -0,0 +1,17 @@
+package com.cursoloiane.matheus.aulas;
+
+public class LoopFor {
+
+	public static void main(String[] args) {
+		/* Exemplo visto na aula sobre For */
+
+		for (int cont = 0; cont < 10; cont++) {
+			System.out.println("valor do contador: " + cont);
+		}
+		for (int cont = 10; cont > 0; cont--) {
+			System.out.println("valor do contador: " + cont);
+		}
+
+	}
+
+}
