b02264a9575cb1f16a2aaf2ce4361c84e292151e
MatheusMamedio <matheusmamedio5@gmail.com>
2023-06-12 22:22:38 -0300

    adicionado exemplo sobre do...while visto na aula

---
+++ Learning_Java/src/com/cursoloiane/matheus/aulas/DoWhile.java
@@ -0,0 +1,19 @@
+package com.cursoloiane.matheus.aulas;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		/* exemplo visto na aula */
+
+		int i = 1;
+
+		do {
+
+			i++;
+			System.out.println("Valor de i: " + i);
+
+		} while (i < 10);
+
+	}
+
+}
