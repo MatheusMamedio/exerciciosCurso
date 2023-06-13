85ebbd9b9ee95bb21694fc650bc8d66129b4f0ab
MatheusMamedio <matheusmamedio5@gmail.com>
2023-06-12 22:17:52 -0300

    adicionado exemplo visto na aula sobre While

---
+++ Learning_Java/src/com/cursoloiane/matheus/aulas/LoopWhile.java
@@ -0,0 +1,31 @@
+package com.cursoloiane.matheus.aulas;
+
+public class LoopWhile {
+
+	public static void main(String[] args) {
+		/*exemplo visto durante a aula*/
+		
+		
+		int i = 1;
+		int max = 10;
+		
+		System.out.println("Contando até " + max);
+		
+		while (i < max) {
+			
+			System.out.println("Valor de i: "+ i);
+			i++;
+		}
+		System.out.println(i);
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
